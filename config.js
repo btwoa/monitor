e55ded989d99d8734a31e0840992f549df2d2b79
btwoa <asciiurl@gmail.com>
2022-08-15 16:56:38 +0800

    🔰

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '站点监测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1828348-038cde293bd4a78a9a8382f6',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://btwoa.com/'
+    },
+    {
+      text: '博客',
+      url: 'https://blog.btwoa.com/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/imsyy/'
+    },
+  ],
+};
