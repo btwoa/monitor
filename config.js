e7f74adde04187ee12d7d95d6167dc4f4dbf3bde
btwoa <109867618+btwoa@users.noreply.github.com>
2022-10-13 23:02:33 +0800

    ⭐

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur1828348-038cde293bd4a78a9a8382f6',
+    'ur1828348-51bad876019a0d0cd2cf7dcd',
   ],
 
   // 日志天数
